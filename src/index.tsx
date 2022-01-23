685006bc3c9813bcf278ce3b864051b1d094d771
NITIN TYAGI <nitintyagi@NITINs-MacBook-Pro.local>
2022-01-23 22:00:44 +0530

    added readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.scss';
 ReactDOM.render(
   <React.StrictMode>
     <App />
